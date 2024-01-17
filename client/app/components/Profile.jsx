0249ed3f380a4d1cbd94774185b8e716056aad9d
Nirjhar <nirjharsingha@gmail.com>
2024-01-17 17:22:22 +0600

    component mount is handled and navigation is done

--- client/app/components/Profile.jsx
+++ client/app/components/Profile.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import { MdEdit } from "react-icons/md";
+import Image from "next/image";
 
 const Profile = () => {
   const [password, setPassword] = useState("");
@@ -37,13 +38,39 @@ const Profile = () => {
         maxHeight: "calc(100svh - 4.1rem)",
       }}
     >
-      <div className="flex justify-center items-center mb-3">
+      <button className="btn btn-circle btn-outline absolute right-1 top-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+      <div className="flex justify-center items-center mb-3 mt-2">
         <BsFillPersonFill className="mr-2 text-3xl text-gray-700" />
         <p className="font-serif text-2xl font-bold text-gray-700">Profile</p>
       </div>
       <div className="overflow-x-hidden">
         <div className="w-full flex justify-center items-center flex-col">
-          <div className="w-[8.5rem] h-[8.5rem] bg-slate-200 mt-1 mb-3 rounded-full border-2 border-solid border-white"></div>
+          {imgStream === "" ? (
+            <div className="w-[8.5rem] h-[8.5rem] bg-slate-200 mt-1 mb-3 rounded-full border-2 border-solid border-white" />
+          ) : (
+            <Image
+              src={imgStream}
+              alt="profile picture"
+              width={136}
+              height={136}
+              className="bg-slate-200 min-w-[8.5rem] min-h-[8.5rem] max-w-[8.5rem] max-h-[8.5rem] mt-1 mb-3 rounded-full border-2 border-solid border-white object-cover"
+            />
+          )}
           {isEdit && (
             <div className="w-full flex justify-center items-center mb-2">
               <div
@@ -54,25 +81,33 @@ const Profile = () => {
                   Choose image
                 </div>
                 <div className="w-[60%] h-full text-gray-700 flex justify-center items-center">
-                  No image chosen
+                  {imgStream === "" ? "No image chosen" : "Image chosen"}
                 </div>
               </div>
-              <div className="w-5 h-5 rounded-full bg-white border-2 border-solid border-gray-700 flex justify-center items-center ml-1 hover:text-white hover:bg-gray-700 cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-3 w-3"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+              {imgStream !== "" && (
+                <div
+                  className="w-5 h-5 rounded-full bg-white border-2 border-solid border-gray-700 flex justify-center items-center ml-1 hover:text-white hover:bg-gray-700 cursor-pointer"
+                  onClick={() => {
+                    setImgStream("");
+                    setProfilePic({});
+                  }}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-3 w-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+              )}
             </div>
           )}
           <input
@@ -112,7 +147,6 @@ const Profile = () => {
               value={username}
               onChange={(e) => {
                 setUsername(e.target.value);
-                setProfilePic("");
               }}
             />
           ) : (
--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GlobalsProvider from "./contexts/Globals";
+import NavBar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <GlobalsProvider>{children}</GlobalsProvider>
+        <GlobalsProvider>
+          <div className="overflow-hidden">
+            <NavBar />
+            {children}
+          </div>
+        </GlobalsProvider>
       </body>
     </html>
   );
